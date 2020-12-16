ddaaa8aa921c5c78801a8db31ed4af9edc9a4af9
mhoc4 <acmjpf@gmail.com>
2020-12-16 08:05:01 -0300

    installed ngrx

---
+++ src/app/pages/home/state/home.reducer.ts
@@ -0,0 +1,25 @@
+import { createReducer, Action, on } from "@ngrx/store"
+
+import * as fromHomeActions from './home.actions';
+
+export interface HomeState {
+    text: string;
+    text2: string;
+}
+
+export const homeInitialState: HomeState = {
+    text: 'Assis',
+    text2: 'QWE',
+}
+
+const reducer = createReducer (
+    homeInitialState,
+    on(fromHomeActions.changeText, (state, { text }) => ({
+        ...state,
+        text,
+    })),
+);
+
+export function homeReducer(state: HomeState | undefined, action: Action): HomeState {
+    return reducer(state, action);
+}
